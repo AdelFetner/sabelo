18269fe362b5d61f862aed8b8f73911b2d5d2d0f
Adel Fetner <adelfetner@gmail.com>
2025-04-14 09:28:54 -0300

    chore: added dependency for gql, modified path on app module, renamed and modified path on typings watcher

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,17 +4,22 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
+      typePaths: ['./src/**/schema/*.graphql'],
       definitions: {
-        path: join(process.cwd(), 'src/graphql.ts'),
+        path: join(process.cwd(), 'src/graphql.schema.ts'),
         outputAs: 'class',
       },
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- server/utils/generate-typings.ts
+++ server/utils/generate-typings.ts
@@ -3,7 +3,9 @@ import { join } from 'path';
 
 const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate({
-  typePaths: ['./src/**/*.graphql'],
-  path: join(process.cwd(), 'src/graphql.ts'),
+  typePaths: ['./src/**/schema/*.graphql'],
+  path: join(process.cwd(), 'src/graphql.schema.ts'),
   outputAs: 'class',
+  watch: true,
+  emitTypenameField: true,
 });
